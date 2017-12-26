1e6ecb59e60fa202ed689d82601286ffc76ae77f
Olena Ageyeva <eageeva@hotmail.com>
2017-12-26 01:27:57 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,7 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet=Pet.create(params[:pet])
+    binding.pry
     @pet.owner=Owner.create(name: params["owner_name"]) if !params["owner_name"].empty?
     @pet.save
 
